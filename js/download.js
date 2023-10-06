8e2f56fb4bf7247a94f3e53e907f30972736e692
FuHao <104718261+FuhaoWaterMelon@users.noreply.github.com>
2023-10-06 15:00:37 +0800

    新建下载模板（为了加速下载）

---
+++ js/download.js
@@ -0,0 +1,7 @@
+function download_link(link ){
+	code = '
+		<a href="/file/扫雷[dist-v1.0.0].zip" download="扫雷[dist-v1.0.0].zip" style="text-decoration: underline">
+						扫雷[dist-v1.0.0].zip
+					</a>
+		';
+}
