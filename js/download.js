6957d420811a098aa5aec22aa1977d284694fa53
FuHao <104718261+FuhaoWaterMelon@users.noreply.github.com>
2024-01-01 07:44:52 +0800

    更新下载源函数

--- js/download.js
+++ js/download.js
@@ -1,7 +1,7 @@
 function download_link(link, name){
 	code = '<a href="https://github.moeyy.xyz/\
 		https://github.com/FuhaoWaterMelon/FuhaoWaterMelon.github.io/blob/main/\
-		'+link+'" download="'+name+'" style="text-decoration: underline">\
+		'+link+'" download style="text-decoration: underline">\
 		'+name+'</a>';
 	document.write(code);
 }
